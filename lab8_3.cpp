9e144652364a737d9c8cdb72f86ade2a92035740
AkanaNDE <Nackawat@gmail.com>
2023-12-12 16:42:06 +0700

    complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,43 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int h;
+    cout << "Enter your age: ";
+    cin >> a;
+if(a > 60){
+cout << "Your character = Brook";
+}
+else if(a>25){
+    cout << "Enter your bounty: ";
+    cin >> b;
+    if (b > 500000000){
+        cout << "Your character = Jinbe";
+    }else{
+        cout << "Your character = Franky";
+    }
+}
+else{
+    cout << "Enter your height: ";
+    cin >> h;
+    if(h < 100){
+        cout << "Your character = Chopper";
+    }
+    else if(h<180){
+        cout << "Your character = Usopp";
+    }
+    else{
+        cout << "Enter your bounty: ";
+    cin >> b;
+    if(b>1100000000){
+          cout << "Your character = Zoro";
+    }
+    else{
+          cout << "Your character = Sanji";
+    }
+    }
+}
+}
